b0375403ecb79b1c1ee956549732858507ddd252
root <root@28c2cc615bac>
2026-03-25 11:34:19 +0000

    Автоматический коммит: обновление файлов

--- src/main/java/com/example/tracingdemo/DemoController.java
+++ src/main/java/com/example/tracingdemo/DemoController.java
@@ -13,29 +13,31 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
 
     private final Logger log = LoggerFactory.getLogger(DemoController.class);
-
+    private final Tracer tracer;
 
     public DemoController(Tracer tracer) {
         this.tracer = tracer;
     }
-
-    private final Tracer tracer;
 
     @GetMapping("/hello")
     public String hello() {
         log.info("hello start");
         Span span = tracer.nextSpan().name("custom-hello-span").start();
         try (Tracer.SpanInScope scope = tracer.withSpan(span)) {
-            Thread.sleep(100); // Имитация обработки
+            simulateProcessing();
             log.info("hello okay");
             return "Hello with tracing!";
         } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-            log.error("error {}", e.getMessage());
+            Thread.currentThread().interrupt(); // Восстанавливаем состояние прерывания
+            log.error("Thread was interrupted", e);
             return "Error";
         } finally {
             log.info("hello finally");
             span.end();
         }
+    }
+
+    private void simulateProcessing() throws InterruptedException {
+        Thread.sleep(100); // Имитация обработки
     }
 }
